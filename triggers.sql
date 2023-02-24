97d545501791cd498b1d6c30db97cbf8fb35c9ed
Bach <bachtom125@gmail.com>
2023-02-24 23:25:59 +0700

    Trigger Addition

--- triggers.sql
+++ triggers.sql
@@ -1,22 +1,47 @@
--- Validation Triggers 
-create or replace function validate_borrowline()
+-- -- Validation Triggers 
+-- create or replace function validate_borrowline()
+-- returns trigger as 
+-- $$
+-- begin	
+-- 	update borrows
+-- 	set total_cost = total_cost + new.price + new.late_fee
+-- 	where borrow_id = new.borrow_id;
+	
+-- 	return new;
+-- end;
+-- $$
+-- language plpgsql;
+
+-- create trigger borrowlines_validate_trigger before insert or update on borrowlines
+-- for each row execute function validate_borrowline();
+
+-- Calculate Late fee
+create or replace function calculate_late_fee()
 returns trigger as 
 $$
-begin	
-	update borrows
-	set total_cost = total_cost + new.price + new.late_fee
-	where borrow_id = new.borrow_id;
-	
-	return new;
+declare 
+	number_of_late_days integer;
+begin
+	if (new.return_date > new.due_date) then
+		number_of_late_days := extract (epoch from (new.shift_end - new.shift_from))/86400;
+		
+		update borrowlines
+		set new.late_fee = number_of_late_days
+		where borrowline_id = new.borrowline_id;
+	end if;
 end;
 $$
 language plpgsql;
 
-create trigger borrowlines_validate_trigger before insert or update on borrowlines
-for each row execute function validate_borrowline();
+create trigger get_late_fee
+after update on borrowlines
+for each row
+execute function calculate_late_fee();
 
--- Create new staff
-create or replace function add_person() returns trigger as $$
+-- Create new staff or customer
+create or replace function add_person() 
+returns trigger as 
+$$
 begin
   if new.role = 'staff' then
     insert into staff (staff_id, person_id, base_salary, hire_date)
@@ -37,16 +62,53 @@ after insert on people
 for each row
 execute function add_person();
 
--- Update total_spending of customer after each borrow, and update ranking if necessary
+-- Add a new book to the library
+create or replace function add_book(
+ 	book_ISBN varchar(20),
+    book_title varchar(50),
+    book_description varchar(100),
+    book_author_name varchar(50),
+    book_category varchar(50),
+    book_publisher_name varchar(50),
+    book_number_of_pages int,
+    book_language_code varchar(3),
+    book_customer_rating numeric(2,2),
+    book_cur_quantity int) 
+returns void as 
+$$
+begin
+	if not exists (select 1 from books where ISBN = new.ISBN) then
+		insert into books (ISBN, title, description, author_name, category, publisher_name, number_of_pages, language_code, customer_rating, cur_quantity)
+  		values (book_ISBN, book_title, book_description, book_author_name, book_category, book_publisher_name, book_number_of_pages, book_language_code, book_customer_rating, book_cur_quantity);
+	else
+	update books 
+	set cur_quantity = cur_quantity + book_cur_quantity
+	where ISBN = book_ISBN;
+	end if;
+end;
+$$ language plpgsql;
+
+-- Update sale_off amount and total_spending of customer after each borrow, and update ranking if necessary
 create or replace function update_customer_ranking()
 returns trigger as 
 $$
 declare
     new_ranking varchar(20);
 begin
+	-- update discount, 20% if platinum, 10% if gold	
+	if ((select ranking from customers where customer_id = new.customer_id) = 'gold') then
+		update borrowlines
+		set sale_off = 0.1 * new.price
+		where borrowline_id = new.borrowline_id;
+	elsif ((select ranking from customers where customer_id = new.customer_id) = 'platinum') then
+		update borrowlines
+		set sale_off = 0.2 * new.price
+		where borrowline_id = new.borrowline_id;
+	end if;
+	
     -- update total_spendings for the borrowing customer
     update customers
-    set total_spendings = total_spendings + new.total_cost
+    set total_spendings = total_spendings + new.price - new.sale_off
     where customer_id = new.customer_id;
     
     -- determine new ranking based on total_spendings
@@ -70,7 +132,7 @@ $$
 language plpgsql;
 
 create trigger update_customer_ranking_trigger
-after insert on borrows
+after insert on borrowlines
 for each row
 execute function update_customer_ranking();
 
@@ -121,4 +183,35 @@ before insert on borrowlines
 for each row
 execute function check_book_availability();
 
--- Addin trigger
+-- Calculate Final Wage of a Shift
+create or replace function calculate_final_wage()
+returns trigger as 
+$$
+declare 
+	hourly_rate integer;
+	working_hours numeric(10, 2);
+begin
+	if (new.week_day >= 7) then -- weekend: 20$/hour
+		hourly_rate := 20;
+	else 
+		hourly_rate := 15;
+	end if;
+	
+	update shifts
+	set wage_per_hr = hourly_rate
+	where shift_id = new.shift_id;
+	
+	working_hours := extract (epoch from (new.shift_end - new.shift_from))/3600;
+	update shifts
+	set wage_final = hourly_rate * working_hours
+	where shift_id = new.shift_id;
+	
+	return new;
+end;
+$$
+language plpgsql;
+
+create trigger get_final_wage
+after insert on shifts
+for each row
+execute function calculate_final_wage();
